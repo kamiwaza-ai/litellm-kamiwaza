d782e3dad2afdda38e84b098061bec67c1de8aec
Matt Wallace <git@savagetech.com>
2025-04-21 12:59:36 -0600

    update with fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="litellm-kamiwaza",
     packages=find_packages(),
     install_requires=[
-        "litellm>=1.0.0",
-        "kamiwaza>=0.3.3.0", # This package provides the kamiwaza_client module
+        "litellm>=1.6.7",
+        "kamiwaza>=0.3.3", # This package provides the kamiwaza_client module
     ],
 )
